c5322746b267cf40e37b5d51aa1280327114f55e
mano-coder <bis.anouar5@gmail.com>
2026-02-12 22:28:28 +0100

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,59 @@
+import { useState } from 'react'
+import arrowIcon from "./assets/images/icon-arrow.svg";
+
+
 function App() {
+  const [ day, setDay ] = useState('')
+  const [ month, setMonth ] = useState('')
+  const [ year, setYear ] = useState('')
 
+  const handleSubmit = (e) => {
+  }
   return (
-    <main class="form-container">
-      <form id="age-form">
-        <div class="input-row">
-          <div class="input-group">
-            <label for="day">Day</label>
+    <main className="form-container">
+      <form id="age-form" onSubmit={handleSubmit}>
+        <div className="input-row">
+          <div className="input-group">
+            <label htmlFor="day">Day</label>
             <input type="number" id="day" name="day" placeholder="DD" />
-            <small class="error-msg"></small>
+            <small className="error-msg"></small>
           </div>
 
-          <div class="input-group">
-            <label for="month">Month</label>
+          <div className="input-group">
+            <label htmlFor="month">Month</label>
             <input type="number" id="month" name="month" placeholder="MM" />
-            <small class="error-msg"></small>
+            <small className="error-msg"></small>
           </div>
 
-          <div class="input-group">
-            <label for="year">Year</label>
+          <div className="input-group">
+            <label htmlFor="year">Year</label>
             <input type="number" id="year" name="year" placeholder="YYYY" />
-            <small class="error-msg"></small>
+            <small className="error-msg"></small>
           </div>
         </div>
 
-        <div class="divider-container">
-          <hr class="line" />
-          <button type="submit" class="submit-btn" aria-label="Calculate Age">
-            <img src="./assets/images/icon-arrow.svg" alt="arrow-icon" />
+        <div className="divider-container">
+          <hr className="line" />
+          <button
+            type="submit"
+            className="submit-btn"
+            aria-label="Calculate Age"
+          >
+            <img src={arrowIcon} alt="arrow-icon" />
           </button>
         </div>
       </form>
 
       <section id="results">
-        <h1><span id="year-result">--</span> years</h1>
-        <h1><span id="month-result">--</span> months</h1>
-        <h1><span id="day-result">--</span> days</h1>
+        <h1>
+          <span id="year-result">--</span> years
+        </h1>
+        <h1>
+          <span id="month-result">--</span> months
+        </h1>
+        <h1>
+          <span id="day-result">--</span> days
+        </h1>
       </section>
     </main>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer class="attribution">
+      Challenge by
+      <a
+        href="https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q"
+        target="_blank"
+        >Frontend Mentor</a
+      >. Coded by <a href="https://github.com/mano-coder">mano-coder</a>.
+    </footer>
+  )
+}
